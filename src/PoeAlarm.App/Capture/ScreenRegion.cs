59e0cd37e368af8e6b8d458be425354b246ebd2e
SouNdmys <soundmys1994@gmail.com>
2026-08-14 18:43:40 +0800

    chore: retire .NET implementation; Rust workspace is the sole codebase going forward

--- src/PoeAlarm.App/Capture/ScreenRegion.cs
+++
@@ -1,8 +0,0 @@
-namespace PoeAlarm.App.Capture;
-
-public readonly record struct ScreenRegion(int X, int Y, int Width, int Height)
-{
-    public bool IsValid => Width > 0 && Height > 0;
-
-    public override string ToString() => $"X={X}, Y={Y}, {Width} × {Height}";
-}
